e919d0f23344650a77b313cb87addaf24ffd6bea
dioh0922 <diohound@gmail.com>
2025-10-19 12:38:18 +0900

    プリフライト修正

--- extApi/callGemini.php
+++ extApi/callGemini.php
@@ -2,13 +2,13 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
-require_once __DIR__ . '/vendor/autoload.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204); // No Content
     exit();
 }
 
+require_once __DIR__ . '/vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
